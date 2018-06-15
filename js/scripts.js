f5d3cad81bf68a532995bd7bc5bd5b6a69f7490e
cc <curt.caldwell24@gmail.com>
2018-06-15 14:55:26 -0700

    doing the logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,49 @@
 
-//var size = 0;
+function Ticket(name, size, topping) {
+  this.name = name;
+  this.movie = size;
+  this.type = topping;
+}
+Ticket.prototype.price = function () {
+  if (this.movie == 
 
-function Pizza(pizzasize) {
-  this.pizzasize = pizzasize ;
-  this.pizzaingredients = [];
-};
-
-// Price Prototype Method
-Pizza.prototype.price = function() {
-  if (this.pizzasize === "Large") {
-    return 21;
-  //  return size * 3;
-} else if (this.pizzasize === "Medium") {
-    return 14;
-  } else {
-    return 7;
-  }
-  if (this.pizzaingredients.length === 0) {
-  price *= 1;
-} else {
-  price += this.pizzaingredients.length;
+  alert( "The cost of your pizza is:$" + parseInt(this.movie + this.type + this.time) + ".00");
 }
 
-return price;
-};
 
 function resetFields() {
-  $("select#pizza-size").val();
+  $("input#name").val();
   $("select#movie-name").val();
-
-
-
+  $("select#ticket-type").val();
+  $("select#ticket-time").val();
+  $("input#ticket-date").val();
 }
 
-// Front-End User Logic
-$(document).ready(function() {
-  $("form#pizza-order-form").submit(function(event) {
-    event.preventDefault();
+$(function () {
+var total = 0;
+$("form#ticket-form").submit(function(event) {
+  event.preventDefault();
 
-    var inputtedPizzaSize = $("select#pizza-size").val();
-    var newPizza = new Pizza(inputtedPizzaSize);
+  var nameinput = $("input#name").val();
+  var movieinput = $("select#movie-name").val();
+  var typeinput = $("select#ticket-type").val();
+  var timeinput = $("select#ticket-time").val();
+  var dateinput = $("input#ticket-date").val();
 
-    $.each($("input[name='toppings']:checked"), function() {
-      newPizza.pizzaingredients.push($(this).val());
-    });
+  var newTicket = new Ticket(nameinput, movieinput, typeinput, timeinput, dateinput)
 
-    $("ul#pizza-order-list").append("<li><button type='submit' class='btn btn-primary btn-margin'><span class='pizzaOrder'>" + newPizza.pizzasize + " Pizza Order" + "</button></span></li>");
+$("ul#purchased").append("<li><span class='ticket'>" + newTicket.name + "</span></li>")
 
-    $(".pizzaOrder").last().click(function() {
-      $("#pizza-order-detail").show();
-      $(".pizza-size").text(newPizza.pizzasize);
-      $(".pizza-ingredients").text(newPizza.pizzaingredients);
-      $(".order-total").text(newPizza.price());
-      alert(inputtedPizzaSize);
+$(".ticket").last().click(function() {
+  $("#show-ticket").show();
+  $("show-ticket h2").text(newTicket.name);
+//  $(".mname").text(newTicket.name);
+  $(".mchoice").text(newTicket.movie);
+  $(".mtype").text(newTicket.price());
+  $(".mtime").text(newTicket.time);
+  $(".mdate").text(newTicket.date);
+});
+resetFields();
 
-    });
-    resetFields();
-  });
+});
 });
