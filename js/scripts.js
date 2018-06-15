0d98539177547da1f157791ee925aa077006bbcc
cc <curt.caldwell24@gmail.com>
2018-06-15 12:15:17 -0700

    redid all html

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,52 @@
+// Business Logic
 
-//Constructor
-function Pizza (name, toppings, size) {
-  this.customerName = name;
-  this.toppings = toppings;
-  this.size = size;
-}
-//Toppings
-var pepperoni = {name: "pepperoni", price: 0.50};
-var mushroom = {name: "mushroom", price: 0.75};
-var onion = {name: "onion", price: 0.25};
-var sausage = {name: "sausage", price: 1.00};
-var bacon = {name: "bacon", price: 1.50};
-var pineapple = {name: "pineapple", price: 0.60};
-var olive = {name: "olive", price: .50};
-var extraCheese = {name: "extraCheese", price: 1.00};
+// Pizza Constructor
+function Pizza(size) {
+  this.pizzasize = size;
+  this.pizzaingredients = [];
+};
 
-//Sizes
-var personal = {name: "personal", price: 5.00, diameter: 8}
-var small = {name: "small", price: 6.00, diameter: 10};
-var medium = {name: "medium", price: 7.00, diameter: 12};
-var large = {name: "large", price: 9.00, diameter: 14};
-var extraLarge = {name: "extraLarge", price: 11.00, diameter: 16};
+// Price Prototype Method
+Pizza.prototype.price = function() {
+  var price = 7;
 
-//Prototype Method for Cost
-Pizza.prototype.cost  = function(){
-  return size.price + toppings.price;
-}
+  if (this.pizzasize === "Large") {
+    price *= 3;
+  } else if (this.pizzasize === "Medium") {
+    price *= 2;
+  } else {
+    price *= 1;
+  }
+
+  if (this.pizzaingredients.length === 0) {
+    price *= 1;
+  } else {
+    price += this.pizzaingredients.length;
+  }
+
+  return price;
+};
+
+// Front-End User Logic
+$(document).ready(function() {
+  $("form#pizza-order-form").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedPizzaSize = $("select#pizza-size").val();
+    var newPizza = new Pizza(inputtedPizzaSize);
+
+    $.each($("input[name='toppings']:checked"), function() {
+      newPizza.pizzaingredients.push($(this).val());
+    });
+
+    $("ul#pizza-order-list").append("<li><button type='submit' class='btn btn-primary btn-margin'><span class='pizzaOrder'>" + newPizza.pizzasize + " Pizza Order" + "</button></span></li>");
+
+    $(".pizzaOrder").last().click(function() {
+      $("#pizza-order-detail").show();
+      $(".pizza-size").text(newPizza.pizzasize);
+      $(".pizza-ingredients").text(newPizza.pizzaingredients);
+      $(".order-total").text(newPizza.price());
+
+    });
+  });
+});
